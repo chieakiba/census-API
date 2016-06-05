65cf6be4e3cd709c17c588b164572dcbcfbe9f72
Chie <akiba.chie@gmail.com>
2016-06-05 10:48:41 -0700

    Created dropdown button function to reflect user choice. Made global variables to acquire user input and dropdown choices

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+//global variable
+var demographicOption;
+var genderOption;
+var ageOption;
+var userEntry = $('.user-entry').val();
+
 $(document).ready(function(){
-	
+	$('.demographic li').click(function(){
+		demographicOption = $(this).html();
+		$('#demographic-button').text(demographicOption);
+	})
+	$('.gender li').click(function(){
+		genderOption = $(this).html();
+		$('#gender-button').text(genderOption);
+	})
+	$('.age li').click(function(){
+		ageOption = $(this).html();
+		$('#age-button').text(ageOption);
+	})
 })
