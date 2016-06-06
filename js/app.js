226d7289ec089a6818763a27ddb98d675e5a93e0
Chie <akiba.chie@gmail.com>
2016-06-06 14:09:30 -0700

    Added getRequest function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ var genderOption;
 var ageOption;
 var userEntry = $('.user-entry').val();
 
+//Census API and Key
+function getRequest(){
+	var request = {
+		key: '6639b468c5267a2ea2453bd45b6247b490ad81ee',
+
+	};
+	url = 'http://api.census.gov/data/timeseries/healthins/sahie';
+	$.getJSON(url, params, function(data){
+			showResults(data.items);
+	});
+}
+
 $(document).ready(function(){
 	$('.demographic li').click(function(){
 		demographicOption = $(this).html();
